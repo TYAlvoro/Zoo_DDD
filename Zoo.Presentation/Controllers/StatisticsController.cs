fb25b574d8ff970807423944bc701511c2867b9a
Tamirlan Yakhyaev <ttiakhiaev@edu.hse.ru>
2025-04-20 12:52:31 +0300

    Implemented Presentation.Controllers

--- Zoo.Presentation/Controllers/StatisticsController.cs
+++ Zoo.Presentation/Controllers/StatisticsController.cs
@@ -1,6 +1,21 @@
+using Microsoft.AspNetCore.Mvc;
+using Zoo.Application.Services;
+
 namespace Zoo.Presentation.Controllers;
 
-public class StatisticsController
+[ApiController]
+[Route("api/stats")]
+public sealed class StatisticsController : ControllerBase
 {
-    
+    private readonly ZooStatisticsService _service;
+
+    public StatisticsController(ZooStatisticsService service) =>
+        _service = service;
+
+    [HttpGet]
+    public async Task<IActionResult> Get()
+    {
+        var stats = await _service.GetAsync();
+        return Ok(stats);
+    }
 }

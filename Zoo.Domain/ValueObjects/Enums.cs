04dd310d9a1deeb96c379ba79cf5bc2ffc3ede14
Tamirlan Yakhyaev <ttiakhiaev@edu.hse.ru>
2025-04-20 12:26:31 +0300

    Implemented Domain.ValueObjects

--- Zoo.Domain/ValueObjects/Enums.cs
+++ Zoo.Domain/ValueObjects/Enums.cs
@@ -1,6 +1,5 @@
 namespace Zoo.Domain.ValueObjects;
 
-public class Enums
-{
-    
-}
+public enum Gender { Male, Female }
+public enum AnimalStatus { Healthy, Sick }
+public enum EnclosureType { Carnivore, Herbivore, Bird, Aquarium }
